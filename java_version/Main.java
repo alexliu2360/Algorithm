84dbd77a5d0810015fb35391b249f90724d4d66c
alexliu2360 <alexliu2360@126.com>
2019-07-22 19:52:33 +0800

    Create Main.java

---
+++ java_version/Main.java
@@ -0,0 +1,45 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+
+    public static  void main(String[] args){
+
+        int N = 100000;
+
+        System.out.println("Test for random array, size = " + N + " , random range [0, " + N + "]");
+
+        Integer[] arr1 = SortTestHelper.generateRandomArray(N, 0, N);
+        Integer[] arr2 = Arrays.copyOf(arr1, arr1.length);
+        Integer[] arr3 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort("SelectionSortUsingCompare", arr1);
+        SortTestHelper.testSort("InsertionSort", arr2);
+        SortTestHelper.testSort("OptimisedInsertionSort", arr3);
+
+        System.out.println("-------------");
+
+        System.out.println("Test for more ordered random array, size = " + N + " , random range [0,3]");
+
+        arr1 = SortTestHelper.generateRandomArray(N, 0, 3);
+        arr2 = Arrays.copyOf(arr1, arr1.length);
+        arr3 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort("SelectionSortUsingCompare", arr1);
+        SortTestHelper.testSort("InsertionSort", arr2);
+        SortTestHelper.testSort("OptimisedInsertionSort", arr3);
+
+        System.out.println("-------------");
+
+        int swapTimes = 5;
+
+        System.out.println("Test for nearly ordered random array, size = " + N + " , random range [0, " + N + "]" + ", swap time = " + swapTimes);
+        arr1 = SortTestHelper.generateNearlyOrderedArray(N, swapTimes);
+        arr2 = Arrays.copyOf(arr1, arr1.length);
+        arr3 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort("SelectionSortUsingCompare", arr1);
+        SortTestHelper.testSort("InsertionSort", arr2);
+        SortTestHelper.testSort("OptimisedInsertionSort", arr3);
+    }
+}
